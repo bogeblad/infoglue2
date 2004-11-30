9b5a6852afd511be82c33fda42c9091289519421
jed <jed>
2004-11-30 16:55:37 +0000

    Formatted according to our convention Removed useless boilerplate comments

--- src/java/org/infoglue/cms/applications/common/actions/VelocityServlet.java
+++ src/java/org/infoglue/cms/applications/common/actions/VelocityServlet.java
@@ -5,15 +5,15 @@
  * ===============================================================================
  *
  *  Copyright (C)
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 2, as published by the
  * Free Software Foundation. See the file LICENSE.html for more information.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY, including the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program; if not, write to the Free Software Foundation, Inc. / 59 Temple
  * Place, Suite 330 / Boston, MA 02111-1307 / USA.
@@ -44,9 +44,8 @@ import org.apache.velocity.context.Context;
 
 import webwork.view.velocity.WebWorkVelocityServlet;
 
-
 /**
- * 
+ *
  * This class puts some things into the context object that you should
  * be aware of (check the superclasses as well):
  * <pre>
@@ -55,99 +54,71 @@ import webwork.view.velocity.WebWorkVelocityServlet;
  *
  * @author <a href="mailto:meat_for_the_butcher@yahoo.com">Patrik Nyborg</a>
  */
-public class VelocityServlet extends WebWorkVelocityServlet 
+public class VelocityServlet extends WebWorkVelocityServlet
 {
-  // --- [Constants] -----------------------------------------------------------
+	private static final String PACKAGE_NAMES_INIT_PARAM = "packageNames";
 
-  private static final String PACKAGE_NAMES_INIT_PARAM = "packageNames";
+	private String packageNames[];
 
+	/**
+	 * Performs initialization of this servlet. Called by the servlet container on loading.
+	 *
+	 * @param configuration The servlet configuration to apply.
+	 *
+	 * @exception ServletException
+	 */
+	public void init(ServletConfig configuration) throws ServletException
+	{
+		super.init(configuration);
+		initializePackageNames(configuration.getInitParameter(PACKAGE_NAMES_INIT_PARAM));
+	}
 
+	/**
+	 * @param commaSeparatedPackageNames comma-separareted list of package names.
+	 */
+	private void initializePackageNames(String commaSeparatedPackageNames)
+	{
+		if (commaSeparatedPackageNames == null)
+		{
+			throw new ConfigurationError("web.xml not properly configured, did not contain the " + PACKAGE_NAMES_INIT_PARAM + " init param for the VelocityServlet.");
+		}
 
-  // --- [Attributes] ----------------------------------------------------------
+		final StringTokenizer st = new StringTokenizer(commaSeparatedPackageNames, ",");
+		this.packageNames = new String[st.countTokens()];
+		for (int i = 0; st.hasMoreTokens(); ++i)
+		{
+			this.packageNames[i] = st.nextToken();
+		}
+	}
 
-  //
-  private String packageNames[];
+	/**
+	 * @param locale
+	 */
+	private StringManager getStringManagerChain(Locale locale)
+	{
+		return StringManagerFactory.getPresentationStringManager(this.packageNames, locale);
+	}
 
+	protected Template handleRequest(HttpServletRequest request, HttpServletResponse response, Context context) throws Exception
+	{
+		CmsLogger.logInfo("handleRequest in VelocityServlet");
+		final HttpSession httpSession = request.getSession();
+		CmsLogger.logInfo("httpSession:" + httpSession);
+		final Session session = new Session(httpSession);
 
+		//<todo>this should definitely not be placed here
+		if (session.getLocale() == null)
+		{
+			session.setLocale(java.util.Locale.ENGLISH);
+		}
+		//</todo>
 
-  // --- [Static] --------------------------------------------------------------
-  // --- [Constructors] --------------------------------------------------------
-  // --- [Public] --------------------------------------------------------------
-  // --- [X implementation] ----------------------------------------------------
-  // --- [org.apache.velocity.servlet.VelocityServlet Overrides] ---------------
+		context.put("ui", getStringManagerChain(session.getLocale()));
+		context.put("formatter", new VisualFormatter());
 
-  /** 
-   * Performs initialization of this servlet. Called by the servlet container on loading.
-   *
-   * @param configuration The servlet configuration to apply.
-   *
-   * @exception ServletException
-   */
-  public void init(ServletConfig configuration) throws ServletException {
-    super.init(configuration);
-    initializePackageNames(configuration.getInitParameter(PACKAGE_NAMES_INIT_PARAM));    
-  }
-
-  
-
-  // --- [Package protected] ---------------------------------------------------
-  // --- [Private] -------------------------------------------------------------
-
-  /**
-   *
-   *
-   * @param commaSeparatedPackageNames comma-separareted list of package names.
-   */
-  private void initializePackageNames(String commaSeparatedPackageNames) {
-    if(commaSeparatedPackageNames == null) {
-      throw new ConfigurationError("web.xml not properly configured, did not contain the " + PACKAGE_NAMES_INIT_PARAM + " init param for the VelocityServlet.");
-    }
-
-    final StringTokenizer st = new StringTokenizer(commaSeparatedPackageNames, ",");
-    this.packageNames = new String[st.countTokens()];
-    for(int i=0; st.hasMoreTokens(); ++i) { 
-      this.packageNames[i] = st.nextToken();
-    }
-  }
-
-  /**
-   * @param locale
-   */
-  private StringManager getStringManagerChain(Locale locale) 
-  {
-    return StringManagerFactory.getPresentationStringManager(this.packageNames, locale);
-  }
-
-
-  // --- [Protected] -----------------------------------------------------------
-
-  /**
-   *
-   */
-    protected Template handleRequest(HttpServletRequest request, HttpServletResponse response, Context context) throws Exception 
-    {
-        CmsLogger.logInfo("handleRequest in VelocityServlet");
-        final HttpSession httpSession = request.getSession();
-        CmsLogger.logInfo("httpSession:" + httpSession);
-        final Session session         = new Session(httpSession);
-        
-        //<todo>this should definitely not be placed here
-        if(session.getLocale() == null) 
-        {
-            session.setLocale(java.util.Locale.ENGLISH);
-        }
-        //</todo>
-
-        context.put("ui", getStringManagerChain(session.getLocale()));
-        context.put("formatter", new VisualFormatter());
-        
-        request.setCharacterEncoding("UTF-8");	
+		request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset=UTF-8");
-		
-        return super.handleRequest(request, response, context);
-    }
 
-
-
-  // --- [Inner classes] -------------------------------------------------------
+		return super.handleRequest(request, response, context);
+	}
 }
