f8fa39c6e6b8cca7c04cf19dfca30c5be584722f
frank <frank>
2004-12-01 23:37:42 +0000

    category domain object

---
+++ src/java/org/infoglue/cms/entities/management/Category.java
@@ -0,0 +1,54 @@
+/* ===============================================================================
+ *
+ * Part of the InfoGlue Content Management Platform (www.infoglue.org)
+ *
+ * ===============================================================================
+ *
+ *  Copyright (C)
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License version 2, as published by the
+ * Free Software Foundation. See the file LICENSE.html for more information.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY, including the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc. / 59 Temple
+ * Place, Suite 330 / Boston, MA 02111-1307 / USA.
+ *
+ * ===============================================================================
+ *
+ * $Id: Category.java,v 1.1 2004/12/01 23:37:42 frank Exp $
+ */
+package org.infoglue.cms.entities.management;
+
+import org.infoglue.cms.entities.kernel.IBaseEntity;
+import org.infoglue.cms.entities.management.impl.simple.CategoryImpl;
+
+/**
+ * This is a Category to be used for categorizing Content
+ *
+ * @author Frank Febbraro (frank@phase2technology.com)
+ */
+public interface Category extends IBaseEntity
+{
+	public Integer getCategoryId();
+	public void setCategoryId(Integer s);
+
+	public String getName();
+	public void setName(String s);
+
+	public String getDescription();
+	public void setDescription(String s);
+
+	public boolean isActive();
+	public void setActive(boolean b);
+
+	public Integer getParentId();
+	public void setParentId(Integer i);
+
+	public CategoryVO getValueObject();
+	public void setValueObject(CategoryVO c);
+}
---
+++ src/java/org/infoglue/cms/entities/management/CategoryVO.java
@@ -0,0 +1,108 @@
+/* ===============================================================================
+ *
+ * Part of the InfoGlue Content Management Platform (www.infoglue.org)
+ *
+ * ===============================================================================
+ *
+ *  Copyright (C)
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License version 2, as published by the
+ * Free Software Foundation. See the file LICENSE.html for more information.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY, including the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc. / 59 Temple
+ * Place, Suite 330 / Boston, MA 02111-1307 / USA.
+ *
+ * ===============================================================================
+ *
+ * $Id: CategoryVO.java,v 1.1 2004/12/01 23:37:42 frank Exp $
+ */
+package org.infoglue.cms.entities.management;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import org.infoglue.cms.entities.kernel.Persistent;
+import org.infoglue.cms.util.ConstraintExceptionBuffer;
+import org.infoglue.cms.util.DomainUtils;
+import org.infoglue.cms.util.validators.ValidatorFactory;
+
+/**
+ * This is a Category to be used for categorizing Content
+ *
+ * @author Frank Febbraro (frank@phase2technology.com)
+ */
+public class CategoryVO extends Persistent
+{
+	private Integer categoryId;
+	private String name;
+	private String description;
+    private boolean active = true;
+	private Integer parentId;
+	private List children = new ArrayList();
+
+	public Integer getId()		{ return getCategoryId();}
+
+	public Integer getCategoryId()		{ return categoryId;}
+	public void setCategoryId(Integer i){ categoryId = i;}
+
+	public String getName()			{ return name; }
+	public void setName(String s)	{ name = s; }
+
+	public String getDescription()		{ return description; }
+	public void setDescription(String s){ description = s; }
+
+	public boolean isActive()			{ return active; }
+	public void setActive(boolean b)	{ active = b; }
+
+	public Integer getParentId()		{ return parentId; }
+	public void setParentId(Integer i)	{ parentId = i; }
+
+	public List getChildren()			{ return children; }
+	public void setChildren(List l)		{ children = l; }
+
+	public boolean isRoot()
+	{
+		return getParentId() == null;
+	}
+
+	public ConstraintExceptionBuffer validate()
+	{
+		ConstraintExceptionBuffer ceb = new ConstraintExceptionBuffer();
+		ValidatorFactory.createStringValidator("Category.name", true, 1, 100).validate(name, ceb);
+		ValidatorFactory.createStringValidator("Category.description", false, 255).validate(description, ceb);
+		return ceb;
+	}
+
+	public boolean equals(Object o)
+	{
+		if(super.equals(o))
+		{
+			CategoryVO category = (CategoryVO)o;
+			return DomainUtils.equals(categoryId, category.categoryId)
+					&& DomainUtils.equals(name, category.name)
+					&& DomainUtils.equals(description, category.description)
+					&& DomainUtils.equals(parentId, category.parentId)
+					&& children.size() == category.children.size()
+					&& (active == category.active);
+		}
+
+		return false;
+	}
+
+	public StringBuffer toStringBuffer()
+	{
+		StringBuffer sb = super.toStringBuffer();
+		sb.append(" name=").append(name)
+			.append(" description=").append(description)
+			.append(" active=").append(active)
+			.append(" parentId=").append(parentId)
+			.append(" children.size=").append(children.size());
+		return sb;
+	}
+}
