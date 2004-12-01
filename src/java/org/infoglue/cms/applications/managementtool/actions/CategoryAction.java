355d897403ffe271e7d42be598aee1d280452631
frank <frank>
2004-12-01 23:42:06 +0000

    this action contains the collection of commands used to operate on a category

---
+++ src/java/org/infoglue/cms/applications/managementtool/actions/CategoryAction.java
@@ -0,0 +1,79 @@
+package org.infoglue.cms.applications.managementtool.actions;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.infoglue.cms.applications.common.actions.WebworkAbstractAction;
+import org.infoglue.cms.applications.common.actions.ModelAction;
+import org.infoglue.cms.controllers.kernel.impl.simple.CategoryController;
+import org.infoglue.cms.exception.SystemException;
+import org.infoglue.cms.exception.ConstraintException;
+import org.infoglue.cms.entities.management.CategoryVO;
+import org.infoglue.cms.entities.kernel.Persistent;
+
+/**
+ * @author Frank Febbraro (frank@phase2technology.com)
+ */
+public class CategoryAction extends ModelAction
+{
+	public static final String MAIN = "main";
+
+	private CategoryController controller = CategoryController.getController();
+
+	protected Persistent createModel()	{ return new CategoryVO(); }
+
+	public CategoryVO getCategory()		{ return (CategoryVO)getModel(); }
+
+	public Integer getCategoryId()			{ return getCategory().getCategoryId(); }
+	public void setCategoryId(Integer i)	{ getCategory().setCategoryId(i); }
+
+
+	public String doList() throws SystemException
+	{
+		setModels(controller.findRootCategories());
+		return SUCCESS;
+	}
+
+	public String doNew() throws SystemException
+	{
+		return SUCCESS;
+	}
+
+	public String doEdit() throws SystemException
+	{
+		setModel(controller.findWithChildren(getCategoryId()));
+		return SUCCESS;
+	}
+
+	public String doDisplayTreeForMove() throws SystemException
+	{
+		return SUCCESS;
+	}
+
+	public String doMove() throws SystemException
+	{
+		setModel(controller.moveCategory(getCategoryId(), getCategory().getParentId()));
+		return SUCCESS;
+	}
+
+	public String doSave() throws SystemException, ConstraintException
+	{
+		validateModel();
+		setModel(controller.save(getCategory()));
+		return (getCategory().isRoot())? MAIN : SUCCESS;
+	}
+
+	public String doDelete() throws SystemException
+	{
+		// So we have the parent and know which page to go to
+		setModel(controller.findById(getCategoryId()));
+		controller.delete(getCategoryId());
+
+		return (getCategory().getParentId() == null) ? MAIN : SUCCESS;
+	}
+
+	// Needed as part of WebworklAbstractAction
+	public String doExecute() throws Exception
+	{ return SUCCESS; }
+}
