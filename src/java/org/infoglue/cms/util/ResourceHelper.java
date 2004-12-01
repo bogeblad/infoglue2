e07344f30bba87a1facaa06c1d9f13d5fc819937
frank <frank>
2004-12-01 23:50:46 +0000

    added another method that allows you to specify the class whise class loader will be used.

--- src/java/org/infoglue/cms/util/ResourceHelper.java
+++ src/java/org/infoglue/cms/util/ResourceHelper.java
@@ -28,91 +28,116 @@ import org.infoglue.cms.exception.ConfigurationError;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.io.IOException;
 
 import java.util.Properties;
+import java.net.URL;
 
 import org.apache.log4j.Logger;
+
 
 /**
  * Utility class for loading/manipulating resources (must be accessible from the classpath).
  *
  * @author <a href="mailto:meat_for_the_butcher@yahoo.com">Patrik Nyborg</a>
  */
-public class ResourceHelper
-{
-	// The size of the buffer to use when working with I/O (4 kB).
-	private static final int CHAR_BUFFER_SIZE = 4096;
+public class ResourceHelper {
+  // --- [Constants] -----------------------------------------------------------
 
-	// The string manager for this package.
-	private static final StringManager sm = StringManagerFactory.getSystemStringManager(Constants.PACKAGE_NAME);
+  // The size of the buffer to use when working with I/O (4 kB).
+  private static final int CHAR_BUFFER_SIZE = 4096;
 
-	// The logger for this class.
-	private static final Logger logger = Logger.getLogger(RegexpHelper.class.getName());
+
+
+  // --- [Attributes] ----------------------------------------------------------
+  // --- [Static] --------------------------------------------------------------
+
+  // The string manager for this package.
+  private static final StringManager sm = StringManagerFactory.getSystemStringManager(Constants.PACKAGE_NAME);
+
+  // The logger for this class.
+  private static final Logger logger = Logger.getLogger(RegexpHelper.class.getName());
+
+
+
+  // --- [Constructors] --------------------------------------------------------
+
+  /**
+   * Static class; don't allow instantiation.
+   */
+  private ResourceHelper() {}
+
+
+
+  // --- [Public] --------------------------------------------------------------
+
+  /**
+   * <p>Loads the specified property file into a Properties object.</p>
+   *
+   * <p>Example: <code>ResourceHelper.loadProperties("se/sprawl/cms/CMSProperties.properties");</code></p>
+   *
+   * @param name the name of the resource ("/"-separated path name).
+   */
+  public static synchronized Properties loadProperties(String name) {
+    logger.debug(sm.getString("resource.loadProperties.info", name));
+    try {
+      final Properties properties = new Properties();
+      properties.load(getResourceAsStream(name, ResourceHelper.class));
+      return properties;
+    } catch(Exception e) {
+      throw new ConfigurationError(sm.getString("resource.loadProperties.error", name), e);
+    }
+  }
+
+  /**
+   * <p>Loads the specified resource file and returns the content as a string.</p>
+   *
+   * <p>Example: <code>ResourceHelper.readResource("se/sprawl/cms/CMSSQL.sql");</code></p>
+   *
+   * @param name the name of the resource ("/"-separated path name).
+   */
+  public static synchronized String readResource(String name) {
+    logger.debug(sm.getString("resource.readResource.info", name));
+    try {
+      final StringBuffer sb = new StringBuffer();
+      final Reader reader   = new InputStreamReader(getResourceAsStream(name, ResourceHelper.class));
+      char[] buf            = new char[CHAR_BUFFER_SIZE];
+      int count             = 0;
+      while((count = reader.read(buf, 0, CHAR_BUFFER_SIZE)) > 0) {
+        sb.append(buf, 0, count);
+      }
+      return sb.toString();
+    } catch(Exception e) {
+      throw new ConfigurationError(sm.getString("resource.readResource.error", name), e);
+    }
+  }
 
 	/**
-	 * Static class; don't allow instantiation.
+	 * Find a resource with a variety of fallback ClassLoaders.
+	 *
+	 * @param resourceName The name of the resource to find. ("/"-separated path name)
+	 * @param callingClass The class looking for the resource
+	 * @return The InputStream for the given resource name, or null if one is not found
 	 */
-	private ResourceHelper()
+	public static InputStream getResourceAsStream(String resourceName, Class callingClass)
 	{
-	}
+		URL url = null;
 
-	/**
-	 * <p>Loads the specified property file into a Properties object.</p>
-	 *
-	 * <p>Example: <code>ResourceHelper.loadProperties("se/sprawl/cms/CMSProperties.properties");</code></p>
-	 *
-	 * @param name the name of the resource ("/"-separated path name).
-	 */
-	public static synchronized Properties loadProperties(String name)
-	{
-		logger.debug(sm.getString("resource.loadProperties.info", name));
+		url = Thread.currentThread().getContextClassLoader().getResource(resourceName);
+
+		if (url == null)
+			url = ResourceHelper.class.getClassLoader().getResource(resourceName);
+
+		if (url == null)
+			url = callingClass.getClassLoader().getResource(resourceName);
+
 		try
 		{
-			final Properties properties = new Properties();
-			properties.load(getResourceAsStream(name));
-			return properties;
+			return url != null ? url.openStream() : null;
 		}
-		catch (Exception e)
+		catch (IOException e)
 		{
-			throw new ConfigurationError(sm.getString("resource.loadProperties.error", name), e);
+			return null;
 		}
-	}
-
-	/**
-	 * <p>Loads the specified resource file and returns the content as a string.</p>
-	 *
-	 * <p>Example: <code>ResourceHelper.readResource("se/sprawl/cms/CMSSQL.sql");</code></p>
-	 *
-	 * @param name the name of the resource ("/"-separated path name).
-	 */
-	public static synchronized String readResource(String name)
-	{
-		logger.debug(sm.getString("resource.readResource.info", name));
-		try
-		{
-			final StringBuffer sb = new StringBuffer();
-			final Reader reader = new InputStreamReader(getResourceAsStream(name));
-			char[] buf = new char[CHAR_BUFFER_SIZE];
-			int count = 0;
-			while ((count = reader.read(buf, 0, CHAR_BUFFER_SIZE)) > 0)
-			{
-				sb.append(buf, 0, count);
-			}
-			return sb.toString();
-		}
-		catch (Exception e)
-		{
-			throw new ConfigurationError(sm.getString("resource.readResource.error", name), e);
-		}
-	}
-
-	/**
-	 * Returns an input stream for reading the specified resource.
-	 *
-	 * @param name the name of the resource ("/"-separated path name).
-	 */
-	private static InputStream getResourceAsStream(String name)
-	{
-		return ResourceHelper.class.getClassLoader().getResourceAsStream(name);
 	}
 }
