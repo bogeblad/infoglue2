d7f7c937cf44a25c60fbc1fc018abff7d93f7f13
frank <frank>
2004-12-02 00:05:42 +0000

    added methods to make OQL Queries far easier to execute

--- src/java/org/infoglue/deliver/controllers/kernel/impl/simple/BaseDeliveryController.java
+++ src/java/org/infoglue/deliver/controllers/kernel/impl/simple/BaseDeliveryController.java
@@ -23,19 +23,19 @@
 
 package org.infoglue.deliver.controllers.kernel.impl.simple;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 import org.exolab.castor.jdo.Database;
 import org.exolab.castor.jdo.OQLQuery;
 import org.exolab.castor.jdo.QueryResults;
+import org.exolab.castor.jdo.QueryException;
 
 import org.infoglue.cms.entities.kernel.BaseEntityVO;
 import org.infoglue.cms.entities.kernel.IBaseEntity;
 import org.infoglue.cms.exception.Bug;
 import org.infoglue.cms.exception.SystemException;
 import org.infoglue.cms.util.CmsLogger;
+import org.infoglue.cms.controllers.kernel.impl.simple.CastorDatabaseService;
 
 /**
  * BaseDeliveryController.java
@@ -166,12 +166,132 @@ public abstract class BaseDeliveryController
 		return Collections.unmodifiableList(resultList);
 	}
 
+	//---------------------------------------------------------------------
+	// Dynamic Query specific operations
+	//---------------------------------------------------------------------
+	/**
+	 * Executes a Query with no parameters
+	 *
+ 	 * @param query An OQL Query
+	 * @return A list of the query results as Impls
+	 * @throws SystemException If an error occurs
+	 */
+	protected static List executeQuery(String query) throws SystemException
+	{
+		return executeQuery(query, Collections.EMPTY_LIST);
+	}
+
+	/**
+	 * Executes a Query, also binds the provided parameters
+	 *
+ 	 * @param query An OQL Query
+	 * @param params A List of paramters
+	 * @return A list of the query results as Castor Impl
+	 * @throws SystemException If an error occurs
+	 */
+	protected static List executeQuery(String query, List params) throws SystemException
+	{
+		Database db = beginTransaction();
+
+		try
+		{
+			List results = executeQuery(db, query, params);
+			commitTransaction(db);
+			return results;
+		}
+		catch (Exception e)
+		{
+			CmsLogger.logSevere("Error executing " + query, e);
+			rollbackTransaction(db);
+			throw new SystemException(e.getMessage(), e);
+		}
+	}
+
+	/**
+	 * Executes a Query, also binds the provided parameters
+	 *
+ 	 * @param query An OQL Query
+	 * @param params A List of paramters
+	 * @return A list of the query results as Castor Impl
+	 * @throws SystemException If an error occurs
+	 */
+	protected static List executeQuery(Database db, String query, List params) throws Exception
+	{
+		List results = new ArrayList();
+		results = Collections.list(createQuery(db, query, params).execute(Database.ReadOnly));
+		return results;
+	}
+
+	/**
+	 * Creates an OQLQuery for the provided Database and binds the parameters to it.
+	 *
+	 * @param db The Database to create the OQLQuery on
+	 * @param query The String OQL query
+	 * @param params A List of Objects to bind to the query sequentially
+	 * @return An OQLQuery instance that can be executer
+	 * @throws org.exolab.castor.jdo.QueryException
+	 */
+	protected static OQLQuery createQuery(Database db, String query, List params) throws QueryException
+	{
+		OQLQuery oql = db.getOQLQuery(query);
+		if (params != null)
+			for (Iterator i = params.iterator(); i.hasNext();)
+				oql.bind(i.next());
+
+		return oql;
+	}
+
+	/**
+	 * This method converts a List of entities to a list of value-objects.
+	 */
+	protected static List toVOList(Collection entities) throws SystemException, Bug
+	{
+		List resultVOList = new ArrayList();
+
+		if(entities == null)
+			return Collections.EMPTY_LIST;
+
+		Iterator iterator = entities.iterator();
+		while (iterator.hasNext())
+		{
+			Object o = (Object)iterator.next();
+
+			try
+			{
+				resultVOList.add(o.getClass().getDeclaredMethod("getValueObject", new Class[0]).invoke(o, new Object[0]));
+			}
+			catch(NoSuchMethodException e)
+			{
+				throw new Bug("The object in list was of the wrong type: " + o.getClass().getName() + ". This should never happen.", e);
+			}
+			catch(Exception e)
+			{
+				throw new SystemException("An error occurred when we tried to convert the collection to a valueList. Reason:" + e.getMessage(), e);
+			}
+		}
+
+		return resultVOList;
+	}
+
+	//--------------------------------------------------------------------------
+	// Database/Transaction specific operations
+	//--------------------------------------------------------------------------
+	/**
+	 * Creates a new database and starts a transaction
+	 * @return A reference to a castor database with a new transaction
+	 * @throws SystemException if a database error occurs.
+	 */
+	public static Database beginTransaction() throws SystemException
+	{
+		Database db = CastorDatabaseService.getDatabase();
+		beginTransaction(db);
+		return db;
+	}
 
 
 	/**
-	 * Begins a transaction on the named database
+	 * Begins a transaction on the supplied database
 	 */
-
 	public static void beginTransaction(Database db) throws SystemException
 	{
 		try
@@ -188,7 +308,6 @@ public abstract class BaseDeliveryController
 	/**
 	 * Ends a transaction on the named database
 	 */
-
 	public static void commitTransaction(Database db) throws SystemException
 	{
 		try
@@ -207,7 +326,6 @@ public abstract class BaseDeliveryController
 	/**
 	 * Rollbacks a transaction on the named database
 	 */
-
 	public static void rollbackTransaction(Database db) throws SystemException
 	{
 		try
@@ -228,7 +346,6 @@ public abstract class BaseDeliveryController
 	/**
 	 * Close the database
 	 */
-
 	public static void closeDatabase(Database db) throws SystemException
 	{
 		try
