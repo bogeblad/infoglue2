dbbc0067e559202dc9889a04236aeb65baa02a60
frank <frank>
2004-12-13 19:09:42 +0000

    there is no need for espcaeHTML the throw an Exception

--- src/java/org/infoglue/cms/applications/common/VisualFormatter.java
+++ src/java/org/infoglue/cms/applications/common/VisualFormatter.java
@@ -5,15 +5,15 @@
  * ===============================================================================
  *
  *  Copyright (C)
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 2, as published by the
  * Free Software Foundation. See the file LICENSE.html for more information.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY, including the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program; if not, write to the Free Software Foundation, Inc. / 59 Temple
  * Place, Suite 330 / Boston, MA 02111-1307 / USA.
@@ -37,14 +37,14 @@ public class VisualFormatter
     public VisualFormatter()
     {
     }
-    
+
     public String formatDate(Date date, String pattern)
-    {	
+    {
     	CmsLogger.logInfo("About to convert " + date + " with the pattern " + pattern);
         if(date == null)
             return "";
-            
-        //CmsLogger.logInfo( TimeZone.getDefault().getID() );      
+
+        //CmsLogger.logInfo( TimeZone.getDefault().getID() );
         /*
         SimpleTimeZone pdt = new SimpleTimeZone(-8 * 60 * 60 * 1000, "PST");
         pdt.setStartRule(Calendar.APRIL, 1, Calendar.SUNDAY, 2*60*60*1000);
@@ -58,29 +58,29 @@ public class VisualFormatter
     }
 
 	public String formatDate(Date date, Locale locale, String pattern)
-	{	
+	{
 		CmsLogger.logInfo("About to convert " + date + " with the pattern " + pattern);
 		if(date == null)
 			return "";
-            
+
 		// Format the current time.
 		SimpleDateFormat formatter = new SimpleDateFormat(pattern, locale);
 		String dateString = formatter.format(date);
 		CmsLogger.logInfo("dateString: " + dateString);
 		return dateString;
 	}
-	
+
     public Date parseDate(String dateString, String pattern)
-    {	
+    {
     	CmsLogger.logInfo("About to convert " + dateString + " with the pattern " + pattern);
         if(dateString == null)
             return new Date();
-        
-        Date date = new Date();    
-        
+
+        Date date = new Date();
+
         try
         {
-	        CmsLogger.logInfo( TimeZone.getDefault().getID() );      
+	        CmsLogger.logInfo( TimeZone.getDefault().getID() );
 	        // Format the current time.
 	        SimpleDateFormat formatter = new SimpleDateFormat(pattern);
 	        date = formatter.parse(dateString);
@@ -90,21 +90,21 @@ public class VisualFormatter
         {
         	CmsLogger.logInfo("Error parsing date:" + dateString);
         }
-        
+
         return date;
     }
 
 	public Date parseDate(String dateString, Locale locale, String pattern)
-	 {	
+	 {
 		 CmsLogger.logInfo("About to convert " + dateString + " with the pattern " + pattern);
 		 if(dateString == null)
 			 return new Date();
-        
-		 Date date = new Date();    
-        
+
+		 Date date = new Date();
+
 		 try
 		 {
-			 CmsLogger.logInfo( TimeZone.getDefault().getID() );      
+			 CmsLogger.logInfo( TimeZone.getDefault().getID() );
 			 // Format the current time.
 			 SimpleDateFormat formatter = new SimpleDateFormat(pattern, locale);
 			 date = formatter.parse(dateString);
@@ -114,25 +114,25 @@ public class VisualFormatter
 		 {
 			 CmsLogger.logInfo("Error parsing date:" + dateString);
 		 }
-        
+
 		 return date;
 	 }
-	
+
 	/**
 	 * This method converts all non-standard characters to html-equivalents.
 	 */
-	
-	public final String escapeHTML(String s) throws Exception
+
+	public final String escapeHTML(String s)
 	{
 		if(s == null)
 			return null;
-			
+
 	    StringBuffer sb = new StringBuffer();
 		int n = s.length();
-	    for (int i = 0; i < n; i++) 
+	    for (int i = 0; i < n; i++)
 	    {
 	       	char c = s.charAt(i);
-    	   	switch (c) 
+    	   	switch (c)
 	       	{
 				case '<': sb.append("&lt;"); break;
 	         	case '>': sb.append("&gt;"); break;
@@ -169,13 +169,13 @@ public class VisualFormatter
 	         	case 'Ø': sb.append("&Oslash;");break;
 	         	case 'ß': sb.append("&szlig;");break;
 	         	case 'ù': sb.append("&ugrave;");break;
-	         	case 'Ù': sb.append("&Ugrave;");break;         
-	         	case 'û': sb.append("&ucirc;");break;         
+	         	case 'Ù': sb.append("&Ugrave;");break;
+	         	case 'û': sb.append("&ucirc;");break;
 	         	case 'Û': sb.append("&Ucirc;");break;
 	         	case 'ü': sb.append("&uuml;");break;
 	         	case 'Ü': sb.append("&Uuml;");break;
-	         	case '®': sb.append("&reg;");break;         
-	         	case '©': sb.append("&copy;");break;   
+	         	case '®': sb.append("&reg;");break;
+	         	case '©': sb.append("&copy;");break;
 	         	case '€': sb.append("&euro;"); break;
 	         	*/
 
@@ -184,23 +184,23 @@ public class VisualFormatter
 	   	}
 	   	return sb.toString();
 	}
-	
+
 
 	/**
 	 * This method converts all non-standard characters to html-equivalents.
 	 */
-	
+
 	public final String escapeExtendedHTML(String s)
 	{
 		if(s == null)
 			return null;
-			
+
 		StringBuffer sb = new StringBuffer();
 		int n = s.length();
-		for (int i = 0; i < n; i++) 
+		for (int i = 0; i < n; i++)
 		{
 			char c = s.charAt(i);
-			switch (c) 
+			switch (c)
 			{
 				case '<': sb.append("&lt;"); break;
 				case '>': sb.append("&gt;"); break;
@@ -237,13 +237,13 @@ public class VisualFormatter
 				case 'Ø': sb.append("&Oslash;");break;
 				case 'ß': sb.append("&szlig;");break;
 				case 'ù': sb.append("&ugrave;");break;
-				case 'Ù': sb.append("&Ugrave;");break;         
-				case 'û': sb.append("&ucirc;");break;         
+				case 'Ù': sb.append("&Ugrave;");break;
+				case 'û': sb.append("&ucirc;");break;
 				case 'Û': sb.append("&Ucirc;");break;
 				case 'ü': sb.append("&uuml;");break;
 				case 'Ü': sb.append("&Uuml;");break;
-				case '®': sb.append("&reg;");break;         
-				case '©': sb.append("&copy;");break;   
+				case '®': sb.append("&reg;");break;
+				case '©': sb.append("&copy;");break;
 				case '€': sb.append("&euro;"); break;
 	         	*/
 				case '\'': sb.append("&#146;"); break;
@@ -253,51 +253,51 @@ public class VisualFormatter
 		}
 		return sb.toString();
 	}
-	
+
 	/**
 	 * This method converts all non-standard characters to html-equivalents.
 	 */
-	
+
 	public final String escapeForJavascripts(String s)
 	{
 		if(s == null)
 			return null;
-			
+
 		StringBuffer sb = new StringBuffer();
 		int n = s.length();
-		for (int i = 0; i < n; i++) 
+		for (int i = 0; i < n; i++)
 		{
 			char c = s.charAt(i);
 			if(c == '\'') sb.append("\\'");
 			else sb.append(c);
 		}
-		
+
 		return sb.toString();
 	}
-	
+
 	/**
 	 * This method converts all non-standard characters to html-equivalents.
 	 */
-	
+
 	public final String encode(String s) throws Exception
 	{
 		if(s == null)
 			return null;
-		
+
 		return URLEncoder.encode(s, "UTF-8");
 	}
 
 	/**
 	 * This method converts all non-standard characters to html-equivalents.
 	 */
-	
+
 	public final String encodeURI(String s) throws Exception
 	{
 		if(s == null)
 			return null;
-		
+
 		String encoding = CmsPropertyHandler.getProperty("URIEncoding");
-		
+
 		return URLEncoder.encode(s, encoding);
 	}
 
