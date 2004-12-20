9af4be9c6c1465603a99b1ddf5506c5e0856e7b2
jed <jed>
2004-12-20 17:55:47 +0000

    Removed bad javadoc tag Removed commented-out code.

--- src/java/org/infoglue/cms/applications/publishingtool/actions/ViewPublicationsAction.java
+++ src/java/org/infoglue/cms/applications/publishingtool/actions/ViewPublicationsAction.java
@@ -5,15 +5,15 @@
  * ===============================================================================
  *
  *  Copyright (C)
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 2, as published by the
  * Free Software Foundation. See the file LICENSE.html for more information.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY, including the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program; if not, write to the Free Software Foundation, Inc. / 59 Temple
  * Place, Suite 330 / Boston, MA 02111-1307 / USA.
@@ -35,16 +35,14 @@ import org.infoglue.cms.exception.SystemException;
 
 /**
  * ViewPublicationsAction.java
- * Created on 2002-okt-01 
- * @author Stefan Sik, ss@frovi.com 
+ * Created on 2002-okt-01
+ * @author Stefan Sik, ss@frovi.com
  * ss
- * 
+ *
  */
-public class ViewPublicationsAction extends WebworkAbstractAction 
+public class ViewPublicationsAction extends WebworkAbstractAction
 {
 	private java.lang.Integer repositoryId;
-	//private List contentToPublish;
-	//private List siteNodeToPublish;
 	
 	private RepositoryVO repositoryVO;
 	private List publicationEvents;
@@ -57,38 +55,27 @@ public class ViewPublicationsAction extends WebworkAbstractAction
 
 
 	/**
-	 * This default method fetched all old publications and also 
+	 * This default method fetched all old publications and also
 	 * all events lined up which has to do with publishing.
 	 */
-	
+
 	protected String doExecute() throws Exception
 	{
 		this.repositoryVO      = RepositoryController.getController().getRepositoryVOWithId(this.repositoryId);
 		this.publicationEvents = PublicationController.getPublicationEvents(this.repositoryId);
 		this.editions          = PublicationController.getEditions(this.repositoryId);
-		
+
 		return "success";
 	}
 
-	/** 
+	/**
 	 * Returns the list of events that are up for review.
 	 */
-	
+
 	public List getPublicationEvents()
 	{
 		return this.publicationEvents;
 	}
-	
-/*
- 	protected String doExecute() throws Exception
-	{
-		this.contentToPublish = PublicationController.getContentVersionVOToPublish(repositoryId);
-		this.siteNodeToPublish = PublicationController.getSiteNodeVersionVOToPublish(repositoryId);
-		this.editions = PublicationController.getEditions(repositoryId);
-		
-		return "success";
-	}
-*/	
 
 	public static List getPublicationDetails(Integer publicationId) throws SystemException
 	{
@@ -104,19 +91,6 @@ public class ViewPublicationsAction extends WebworkAbstractAction
 	{
 		return PublicationController.getOwningSiteNodeVO(id);
 	}
-
-	/*
-	public List getPublicationContentVersionVOToPublish(Integer id) throws SystemException
-	{
-		return PublicationController.getPublicationContentVersionVOToPublish(id);
-	}
-
-	public List getPublicationSiteNodeVersionVOToPublish(Integer id) throws SystemException
-	{
-		return PublicationController.getPublicationSiteNodeVersionVOToPublish(id);
-	}
-	*/
-
 
 	/**
 	 * Returns the repositoryId.
@@ -137,17 +111,6 @@ public class ViewPublicationsAction extends WebworkAbstractAction
 	}
 
 	/**
-	 * Returns the contentToPublish.
-	 * @return List
-	 */
-/*
- 	public List getContentToPublish()
-	{
-		return contentToPublish;
-	}
-*/
-
-	/**
 	 * Returns the editions.
 	 * @return List
 	 */
@@ -155,17 +118,6 @@ public class ViewPublicationsAction extends WebworkAbstractAction
 	{
 		return editions;
 	}
-
-	/**
-	 * Sets the contentToPublish.
-	 * @param contentToPublish The contentToPublish to set
-	 */
-/*
- 	public void setContentToPublish(List contentToPublish)
-	{
-		this.contentToPublish = contentToPublish;
-	}
-*/
 
 	/**
 	 * Sets the editions.
@@ -187,30 +139,10 @@ public class ViewPublicationsAction extends WebworkAbstractAction
 
 	/**
 	 * Escapes the string
-	 * @author mattias
 	 */
-	
+
 	public String escape(String string)
 	{
 		return string.replace('\'', '´');
 	}
-
-	/**
-	 * Returns the siteNodeToPublish.
-	 * @return List
-	 */
-/*
- 	public List getSiteNodeToPublish() {
-		return siteNodeToPublish;
-	}
-*/
-	/**
-	 * Sets the siteNodeToPublish.
-	 * @param siteNodeToPublish The siteNodeToPublish to set
-	 */
-/*
-	public void setSiteNodeToPublish(List siteNodeToPublish) {
-		this.siteNodeToPublish = siteNodeToPublish;
-	}
-*/
 }
