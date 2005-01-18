171f4181d06120898114af4569dc7996d4d166b7
jed <jed>
2005-01-18 16:31:14 +0000

    Reworked to allow testing with active instances of differeent workflows. Added testStartWorkflow() Added testExecute2ActiveWorkflows().

--- test/java/org/infoglue/cms/applications/mydesktoptool/actions/ViewMyDesktopToolStartPageActionTest.java
+++ test/java/org/infoglue/cms/applications/mydesktoptool/actions/ViewMyDesktopToolStartPageActionTest.java
@@ -20,63 +20,141 @@
  *
  * ===============================================================================
  *
- * $Id: ViewMyDesktopToolStartPageActionTest.java,v 1.1 2005/01/17 17:54:35 jed Exp $
+ * $Id: ViewMyDesktopToolStartPageActionTest.java,v 1.2 2005/01/18 16:31:14 jed Exp $
  */
 package org.infoglue.cms.applications.mydesktoptool.actions;
 
+import java.util.*;
+
 import org.infoglue.cms.util.*;
 import org.infoglue.cms.applications.mydesktoptool.actions.ViewMyDesktopToolStartPageAction;
+import org.infoglue.cms.applications.common.Session;
+import org.infoglue.cms.controllers.kernel.impl.simple.WorkflowController;
+import org.infoglue.cms.entities.mydesktop.*;
 import webwork.action.ActionContext;
 
 /**
- * Tests ViewMyDesktopToolStartPageAction using the Create News workflow.
  * @author <a href="mailto:jedprentice@gmail.com">Jed Prentice</a>
  */
-public class ViewMyDesktopToolStartPageActionTest extends WorkflowTestCase
+public class ViewMyDesktopToolStartPageActionTest extends WebWorkTestCase
 {
+	private static final WorkflowController controller = WorkflowController.getController();
+
 	private ViewMyDesktopToolStartPageAction action = new ViewMyDesktopToolStartPageAction();
 	private FakeHttpServletRequest request = new FakeHttpServletRequest();
-
-	protected String getWorkflowName()
-	{
-		return "Create News";
-	}
 
 	protected void setUp() throws Exception
 	{
 		super.setUp();
-		setUserPrincipal(getAdminPrincipal());
+		new Session().setInfoGluePrincipal(getAdminPrincipal());
 
 		ActionContext.setRequest(request);
 		action.setServletRequest(request);
 		action.setServletResponse(new FakeHttpServletResponse());
-
-		action.setWorkflowName(getWorkflowName());
-		action.doStartWorkflow();
-		setWorkflow(action.getWorkflow());
 	}
 
 	public void testExecute() throws Exception
 	{
-		WebWorkTestCase.assertSuccess(action.doExecute());
+		assertSuccess(action.doExecute());
 		assertEquals("Wrong number of available workflows:", 2, action.getAvailableWorkflowVOList().size());
-		assertFalse("There should be at least 1 current workflow", action.getWorkflowVOList().isEmpty());
-		assertFalse("There should be at least 1 current action", action.getWorkflowActionVOList().isEmpty());
+	}
+
+	public void testExecute2ActiveWorkflows() throws Exception
+	{
+		WorkflowVO workflow1 = controller.initializeWorkflow(getAdminPrincipal(), "Create News", 0);
+		WorkflowVO workflow2 = controller.initializeWorkflow(getAdminPrincipal(), "Create User", 0);
+
+		try
+		{
+			assertSuccess(action.doExecute());
+			assertEquals("Wrong number of available workflows:", 2, action.getAvailableWorkflowVOList().size());
+			assertTrue("There should be at least 2 current workflows", action.getWorkflowVOList().size() >= 2);
+
+			List availableActions = action.getWorkflowActionVOList();
+			assertTrue("There should be at least 2 current actions", availableActions.size() >= 2);
+			assertContains(availableActions,
+								new WorkflowActionVO(new Integer(4), workflow1.getWorkflowId(), "Create news content"));
+			assertContains(availableActions,
+								new WorkflowActionVO(new Integer(4), workflow2.getWorkflowId(), "Register Name 1"));
+		}
+		finally
+		{
+			finishWorkflow(workflow1.getIdAsPrimitive());
+			finishWorkflow(workflow2.getIdAsPrimitive());
+		}
+	}
+
+	public void testStartWorkflow() throws Exception
+	{
+		action.setWorkflowName("Create News");
+		action.doStartWorkflow();
+
+		try
+		{
+			assertFalse("There should be at least 1 current workflow", action.getWorkflowVOList().isEmpty());
+			List workflowActions = action.getWorkflowActionVOList();
+			assertFalse("There should be at least 1 current action", workflowActions.isEmpty());
+			assertContains(workflowActions,
+								new WorkflowActionVO(new Integer(4), action.getWorkflow().getWorkflowId(), "Create news content"));
+		}
+		finally
+		{
+			finishWorkflow(action.getWorkflow().getIdAsPrimitive());
+		}
 	}
 
 	public void testInvoke() throws Exception
 	{
-		request.setParameter("name", getName());
-		request.setParameter("title", getName());
-		request.setParameter("navigationTitle", getName());
-		request.setParameter("leadIn", getName());
-		request.setParameter("fullText", getName());
+		WorkflowVO workflow = controller.initializeWorkflow(getAdminPrincipal(), "Create News", 0);
 
-		action.setWorkflowId(getWorkflowId());
-		action.setActionId(4);
-		WebWorkTestCase.assertNone(action.doInvoke());
+		try
+		{
+			request.setParameter("name", getName());
+			request.setParameter("title", getName());
+			request.setParameter("navigationTitle", getName());
+			request.setParameter("leadIn", getName());
+			request.setParameter("fullText", getName());
 
-		assertNull("Available workflows should be null:", action.getAvailableWorkflowVOList());
-		assertNull("Current workflows should be null:", action.getWorkflowVOList());
+			action.setWorkflowId(workflow.getIdAsPrimitive());
+			action.setActionId(4);
+			assertNone(action.doInvoke());
+
+			assertNull("Available workflows should be null:", action.getAvailableWorkflowVOList());
+			assertNull("Current workflows should be null:", action.getWorkflowVOList());
+		}
+		finally
+		{
+			finishWorkflow(workflow.getIdAsPrimitive());
+		}
+	}
+
+	private void finishWorkflow(long workflowId) throws Exception
+	{
+		controller.invokeAction(getAdminPrincipal(), request, workflowId, 201);
+	}
+
+	private static void assertContains(List actions, WorkflowActionVO expected)
+	{
+		boolean containsAction = false;
+		for (Iterator i = actions.iterator(); i.hasNext() && !containsAction;)
+			if (isSameAction(expected, (WorkflowActionVO)i.next()))
+				containsAction = true;
+
+		assertTrue("Action " + expected.getId() + " " + expected.getName() + " should be in the list of workflow actions",
+					  containsAction);
+	}
+
+	/**
+	 * A loose check to see if 2 actions are the same.  Compares id, workflowId, and name.  For now, this is sufficient.
+	 * @param one an action
+	 * @param another another action
+	 * @return true if both actions have the same id, workflowId, and name; otherwise returns false.
+	 */
+	private static boolean isSameAction(WorkflowActionVO one, WorkflowActionVO another)
+	{
+		return one.getId().equals(another.getId())
+				&& one.getWorkflowId().equals(another.getWorkflowId())
+				&& one.getWorkflowId().equals(another.getWorkflowId())
+				&& one.getName().equals(another.getName());
 	}
 }
