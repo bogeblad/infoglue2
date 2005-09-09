af426bfeb2ac36a9df621a5caef5597fc41afafc
mattias <mattias.bogeblad@knowit.se>
2005-09-09 07:23:43 +0000

    Added yearsAhead argument (if unspecified the default 10 is used as before)

--- src/java/org/infoglue/cms/applications/workflowtool/function/defaultvalue/ExpireDatePopulator.java
+++ src/java/org/infoglue/cms/applications/workflowtool/function/defaultvalue/ExpireDatePopulator.java
@@ -1,6 +1,7 @@
 package org.infoglue.cms.applications.workflowtool.function.defaultvalue;
 
 import java.util.Calendar;
+import java.util.Map;
 
 import org.infoglue.cms.applications.workflowtool.function.ContentPopulator;
 import org.infoglue.cms.applications.workflowtool.util.ContentValues;
@@ -9,32 +10,95 @@ import org.infoglue.cms.applications.workflowtool.util.ContentValues;
 import com.opensymphony.module.propertyset.PropertySet;
 import com.opensymphony.workflow.WorkflowException;
 
-public class ExpireDatePopulator extends DatePopulator {
+public class ExpireDatePopulator extends DatePopulator
+{
 	/**
 	 * 
 	 */
-	public ExpireDatePopulator() { super();	}
+	private static final String YEARS_AHEAD_ARGUMENT = "yearsAhead";
 	
 	/**
 	 * 
 	 */
-	protected void populate(final PropertySet ps) throws WorkflowException {
+	private static final int DEFAULT_YEARS_AHEAD = 10;
+	
+	/**
+	 * 
+	 */
+	private int yearsAhead = DEFAULT_YEARS_AHEAD;
+	
+	
+	
+	/**
+	 * 
+	 */
+	public ExpireDatePopulator() 
+	{ 
+		super();	
+	}
+	
+	/**
+	 * 
+	 */
+	protected void populate(final PropertySet ps) throws WorkflowException 
+	{
 		final Calendar calendar = Calendar.getInstance();
-		calendar.set(Calendar.YEAR, calendar.get(Calendar.YEAR) + 10);
+		calendar.set(Calendar.YEAR, calendar.get(Calendar.YEAR) + yearsAhead);
 		super.populate(ps, ContentPopulator.PROPERTYSET_CONTENT_PREFIX + ContentValues.EXPIRE_DATE_TIME, calendar.getTime());
 	}
 	
 	/**
 	 * 
 	 */
-	protected void populate(final PropertySet ps, final String name) throws WorkflowException {
+	protected void populate(final PropertySet ps, final String name) throws WorkflowException 
+	{
 		populate(ps);
 	}
 
 	/**
 	 * 
 	 */
-	protected void populate(final PropertySet ps, final String name, final String value) throws WorkflowException {
+	protected void populate(final PropertySet ps, final String name, final String value) throws WorkflowException 
+	{
 		populate(ps);
+	}
+
+	/**
+	 * 
+	 */
+	protected void initialize(final Map transientVars, final Map args, final PropertySet ps) throws WorkflowException 
+	{
+		super.initialize(transientVars, args, ps);
+		initializeYearsAhead(args);
+	}
+	
+	/**
+	 * 
+	 */
+	private void initializeYearsAhead(final Map args)
+	{
+		if(args.containsKey(YEARS_AHEAD_ARGUMENT))
+		{
+			try
+			{
+				int temporary = Integer.parseInt(args.get(YEARS_AHEAD_ARGUMENT).toString());
+				if(temporary > 0)
+				{
+					yearsAhead = temporary;
+					getLogger().debug("Using [" + temporary + "] as years ahead value.");
+				}
+				else
+					getLogger().warn("Illegal years ahead argument [" + temporary + "].");
+						
+			}
+			catch(Exception e)
+			{
+				getLogger().warn("Illegal years ahead argument [" + e.getMessage() + "].");
+			}
+		}
+		else
+		{
+			getLogger().debug("Using default years ahead value.");
+		}
 	}
 }
