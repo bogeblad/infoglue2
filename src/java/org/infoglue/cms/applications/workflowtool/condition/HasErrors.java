ae657376323ba7afe71e669c409eda6ff549c617
mattias <mattias.bogeblad@knowit.se>
2005-09-13 12:57:32 +0000

    Added header + fixed indentation

--- src/java/org/infoglue/cms/applications/workflowtool/condition/HasErrors.java
+++ src/java/org/infoglue/cms/applications/workflowtool/condition/HasErrors.java
@@ -1,10 +1,31 @@
+/* ===============================================================================
+*
+* Part of the InfoGlue Content Management Platform (www.infoglue.org)
+*
+* ===============================================================================
+*
+*  Copyright (C)
+*
+* This program is free software; you can redistribute it and/or modify it under
+* the terms of the GNU General Public License version 2, as published by the
+* Free Software Foundation. See the file LICENSE.html for more information.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY, including the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along with
+* this program; if not, write to the Free Software Foundation, Inc. / 59 Temple
+* Place, Suite 330 / Boston, MA 02111-1307 / USA.
+*
+* ===============================================================================
+*/
 package org.infoglue.cms.applications.workflowtool.condition;
 
 import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.infoglue.cms.applications.workflowtool.function.ErrorPopulator;
-
 
 import com.opensymphony.module.propertyset.PropertySet;
 import com.opensymphony.workflow.Condition;
@@ -13,7 +34,8 @@ import com.opensymphony.workflow.WorkflowException;
 /**
  * 
  */
-public class HasErrors implements Condition {
+public class HasErrors implements Condition 
+{
 	/**
 	 * 
 	 */
@@ -24,13 +46,17 @@ public class HasErrors implements Condition {
 	/**
 	 * 
 	 */
-	public HasErrors() { super(); }
+	public HasErrors() 
+	{ 
+		super(); 
+	}
 
 	/**
 	 * 
 	 */
-	public boolean passesCondition(final Map transientVars, final Map args, final PropertySet ps) throws WorkflowException {
-		final boolean condition = !ps.getKeys(ErrorPopulator.PROPERTYSET_ERROR_PREFIX).isEmpty();
+	public boolean passesCondition(final Map transientVars, final Map args, final PropertySet ps) throws WorkflowException 
+	{
+		final boolean condition = !ps.getKeys(ErrorPopulator.ERROR_PROPERTYSET_PREFIX).isEmpty();
 		logger.debug("HasErrors.passesCondition() : " + condition);
 		return condition;
 	}
--- src/java/org/infoglue/cms/applications/workflowtool/condition/HasStatus.java
+++ src/java/org/infoglue/cms/applications/workflowtool/condition/HasStatus.java
@@ -1,3 +1,25 @@
+/* ===============================================================================
+*
+* Part of the InfoGlue Content Management Platform (www.infoglue.org)
+*
+* ===============================================================================
+*
+*  Copyright (C)
+*
+* This program is free software; you can redistribute it and/or modify it under
+* the terms of the GNU General Public License version 2, as published by the
+* Free Software Foundation. See the file LICENSE.html for more information.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY, including the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along with
+* this program; if not, write to the Free Software Foundation, Inc. / 59 Temple
+* Place, Suite 330 / Boston, MA 02111-1307 / USA.
+*
+* ===============================================================================
+*/
 package org.infoglue.cms.applications.workflowtool.condition;
 
 import java.util.Map;
@@ -9,7 +31,8 @@ import com.opensymphony.module.propertyset.PropertySet;
 import com.opensymphony.workflow.Condition;
 import com.opensymphony.workflow.WorkflowException;
 
-public class HasStatus implements Condition {
+public class HasStatus implements Condition 
+{
 	/**
 	 * 
 	 */
@@ -34,12 +57,16 @@ public class HasStatus implements Condition {
 	/**
 	 * 
 	 */
-	public HasStatus() { super(); }
+	public HasStatus() 
+	{ 
+		super(); 
+	}
 
 	/**
 	 * 
 	 */
-	public boolean passesCondition(final Map transientVars, final Map args, final PropertySet ps) throws WorkflowException {
+	public boolean passesCondition(final Map transientVars, final Map args, final PropertySet ps) throws WorkflowException 
+	{
 		initialize(transientVars, args, ps);
 		logger.debug("HasStatus.passesCondition() : " + wantedStatus.equals(currentStatus));
 		return wantedStatus.equals(currentStatus);
@@ -48,13 +75,15 @@ public class HasStatus implements Condition {
 	/**
 	 * 
 	 */
-	protected void initialize(final Map transientVars, final Map args, final PropertySet ps) throws WorkflowException {
-		if(!args.containsKey(ARGUMENT_WANTED_STATUS)) {
+	protected void initialize(final Map transientVars, final Map args, final PropertySet ps) throws WorkflowException 
+	{
+		if(!args.containsKey(ARGUMENT_WANTED_STATUS)) 
+		{
 			final WorkflowException e = new WorkflowException("The wanted status argument is missing.");
 			logger.error(e.toString());
 			throw e;
 		}
 		wantedStatus  = (String) args.get(ARGUMENT_WANTED_STATUS);
-		currentStatus = (String) (ps.exists(InfoglueFunction.PROPERTYSET_STATUS) ? ps.getString(InfoglueFunction.PROPERTYSET_STATUS) : null); 
+		currentStatus = (String) (ps.exists(InfoglueFunction.STATUS_PROPERTYSET_KEY) ? ps.getString(InfoglueFunction.STATUS_PROPERTYSET_KEY) : null); 
 	}
 }
