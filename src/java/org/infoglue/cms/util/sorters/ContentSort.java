2a4e7603c525b3a81c0cbaa4198a0fd42e1d03b7
mattias <mattias.bogeblad@knowit.se>
2005-09-14 06:51:53 +0000

    Added missing header. Infoglue indentation.

--- src/java/org/infoglue/cms/util/sorters/ContentSort.java
+++ src/java/org/infoglue/cms/util/sorters/ContentSort.java
@@ -1,3 +1,25 @@
+/* ===============================================================================
+ *
+ * Part of the InfoGlue Content Management Platform (www.infoglue.org)
+ *
+ * ===============================================================================
+ *
+ *  Copyright (C)
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License version 2, as published by the
+ * Free Software Foundation. See the file LICENSE.html for more information.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY, including the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc. / 59 Temple
+ * Place, Suite 330 / Boston, MA 02111-1307 / USA.
+ *
+ * ===============================================================================
+ */
 package org.infoglue.cms.util.sorters;
 
 import java.lang.reflect.Constructor;
@@ -17,7 +39,8 @@ import org.infoglue.deliver.controllers.kernel.impl.simple.TemplateController;
 /**
  * 
  */
-class SortComparable implements Comparable {
+class SortComparable implements Comparable 
+{
 	/**
 	 * 
 	 */
@@ -31,38 +54,44 @@ class SortComparable implements Comparable {
 	/**
 	 * 
 	 */
-	public final int compareTo(Object o) {
+	public final int compareTo(final Object o) 
+	{
 		if(!(o instanceof SortComparable))
+		{
 			throw new ClassCastException();
+		}
 		final SortComparable other = (SortComparable) o;
 		if(other.comparables.size() != comparables.size())
+		{
 			throw new IllegalStateException("Trying to compare SortComparable with different number of elements.");
-		for(int i=0; i<comparables.size(); i++) {
+		}
+		for(int i=0; i<comparables.size(); i++) 
+		{
 			final int result = compareTo(other, i);
 			if(result != 0)
+			{
 				return result;
+			}
 		}
-		
 		return 0;
 	}
 	
 	/**
 	 * 
 	 */
-	private final int compareTo(final SortComparable other, final int index) {
+	private final int compareTo(final SortComparable other, final int index) 
+	{
 		final Comparable c1      = (Comparable) comparables.get(index);
 		final Comparable c2      = (Comparable) other.comparables.get(index);
 		final Boolean ascending  = (Boolean) orders.get(index);
-		if(ascending.booleanValue())
-			return c1.compareTo(c2);
-		else
-			return c2.compareTo(c1);
+		return ascending.booleanValue() ? c1.compareTo(c2) : c2.compareTo(c1);
 	}
 	
 	/**
 	 * 
 	 */
-	public final void add(final Comparable c, final boolean ascending) {
+	public final void add(final Comparable c, final boolean ascending) 
+	{
 		comparables.add(c);
 		orders.add(new Boolean(ascending));
 	}
@@ -71,7 +100,8 @@ class SortComparable implements Comparable {
 /**
  * 
  */
-class SortStruct implements Comparable {
+class SortStruct implements Comparable 
+{
 	/**
 	 * 
 	 */
@@ -96,7 +126,8 @@ class SortStruct implements Comparable {
 	/**
 	 * 
 	 */
-	SortStruct(final TemplateController controller, final ContentVO contentVO) {
+	SortStruct(final TemplateController controller, final ContentVO contentVO) 
+	{
 		this.controller = controller;
 		this.contentVO  = contentVO;
 	}
@@ -104,7 +135,8 @@ class SortStruct implements Comparable {
 	/**
 	 * 
 	 */
-	SortStruct(final TemplateController controller, final ContentVersionVO contentVersionVO) {
+	SortStruct(final TemplateController controller, final ContentVersionVO contentVersionVO) 
+	{
 		this.controller       = controller;
 		this.contentVersionVO = contentVersionVO;
 	}
@@ -112,21 +144,24 @@ class SortStruct implements Comparable {
 	/**
 	 * 
 	 */
-	public void addContentProperty(final String name, final boolean ascending) {
+	public void addContentProperty(final String name, final boolean ascending) 
+	{
 		add(getProperty(getContentVO(), name), ascending);
 	}
 
 	/**
 	 * 
 	 */
-	public void addContentVersionProperty(final String name, final boolean ascending) {
+	public void addContentVersionProperty(final String name, final boolean ascending) 
+	{
 		add(getProperty(getContentVersionVO(), name), ascending);
 	}
 
 	/**
 	 * 
 	 */
-	public void addContentVersionAttribute(final String name, final Class clazz, final boolean ascending) {
+	public void addContentVersionAttribute(final String name, final Class clazz, final boolean ascending) 
+	{
 		final Integer contentId  = getContentId();
 		final String stringValue = controller.getContentAttribute(contentId, controller.getLanguageId(), name); 
 		
@@ -136,16 +171,24 @@ class SortStruct implements Comparable {
 	/**
 	 * 
 	 */
-	private Comparable castAttribute(final String name, final Class clazz, final String stringValue) {
+	private Comparable castAttribute(final String name, final Class clazz, final String stringValue) 
+	{
 		if(String.class.equals(clazz))
+		{
 			return stringValue;
-		try {
+		}
+		try 
+		{
 			if(clazz.isAssignableFrom(Comparable.class))
+			{
 				throw new IllegalArgumentException(clazz.getName() + " is not comparable.");
+			}
 			final Constructor ctor = clazz.getConstructor(new Class[] { String.class });
 			final String s = (Number.class.isAssignableFrom(clazz) && stringValue.equals("")) ? "0" : stringValue;
 			return (Comparable) ctor.newInstance(new Object[] { s });
-		} catch(Exception e) {
+		} 
+		catch(Exception e) 
+		{
 			e.printStackTrace();
 			throw new IllegalArgumentException("Unable to cast [" + name + "] to [" + clazz.getName() + "].");
 		}
@@ -154,17 +197,22 @@ class SortStruct implements Comparable {
 	/**
 	 * 
 	 */
-	private Integer getContentId() {
+	private Integer getContentId() 
+	{
 		return (contentVO != null) ? getContentVO().getContentId() : getContentVersionVO().getContentId();
 	}
 	
 	/**
 	 * 
 	 */
-	private Comparable getProperty(final Object o, final String name) {
-		try {
+	private Comparable getProperty(final Object o, final String name) 
+	{
+		try 
+		{
 			return (Comparable) PropertyUtils.getProperty(o, name);
-		} catch(Exception e) {
+		} 
+		catch(Exception e) 
+		{
 			e.printStackTrace();
 			throw new IllegalArgumentException("Illegal property [" + name + "] : " + e);
 		}
@@ -173,34 +221,44 @@ class SortStruct implements Comparable {
 	/**
 	 * 
 	 */
-	private void add(final Comparable c, final boolean ascending) {
+	private void add(final Comparable c, final boolean ascending) 
+	{
 		sortComparable.add(c, ascending);
 	}
 	
 	/**
 	 * 
 	 */
-	ContentVersionVO getContentVersionVO() {
+	ContentVersionVO getContentVersionVO() 
+	{
 		if(contentVersionVO == null) 
+		{
 			contentVersionVO = controller.getContentVersion(contentVO.getContentId());
+		}
 		return contentVersionVO;
 	}
 	
 	/**
 	 * 
 	 */
-	ContentVO getContentVO() {
-		if(contentVO == null) 
+	ContentVO getContentVO() 
+	{
+		if(contentVO == null)
+		{
 			contentVO = controller.getContent(contentVersionVO.getContentId());
+		}
 		return contentVO;
 	}
 
 	/**
 	 * 
 	 */
-	public final int compareTo(Object o) {
+	public final int compareTo(Object o) 
+	{
 		if(!(o instanceof SortStruct))
+		{
 			throw new ClassCastException();
+		}
 		final SortStruct other = (SortStruct) o;
 		return sortComparable.compareTo(other.sortComparable);
 	}
@@ -209,7 +267,8 @@ class SortStruct implements Comparable {
 /**
  * 
  */
-public class ContentSort {
+public class ContentSort 
+{
 	/**
 	 * 
 	 */
@@ -224,7 +283,8 @@ public class ContentSort {
 	/**
 	 * 
 	 */
-	public ContentSort(final TemplateController controller, final Collection elements) {
+	public ContentSort(final TemplateController controller, final Collection elements) 
+	{
 		this.controller = controller;
 		addElements(elements);
 	}
@@ -232,82 +292,116 @@ public class ContentSort {
 	/**
 	 * 
 	 */
-	private final void addElements(final Collection elements) {
+	private final void addElements(final Collection elements) 
+	{
 		if(elements == null || elements.isEmpty())
+		{
 			return;
+		}
 		final Object element = elements.toArray()[0];
 		if(element instanceof Content)
+		{
 			initializeWithContent(elements);
+		}
 		if(element instanceof ContentVO)
+		{
 			initializeWithContentVO(elements);
+		}
 		if(element instanceof ContentVersion)
+		{
 			initializeWithContentVersion(elements);
+		}
 		if(element instanceof ContentVersionVO)
+		{
 			initializeWithContentVersionVO(elements);
+		}
 	}
 	
 	/**
 	 * 
 	 */
-	private final void initializeWithContent(final Collection elements) {
+	private final void initializeWithContent(final Collection elements) 
+	{
 		for(final Iterator i=elements.iterator(); i.hasNext(); )
+		{
 			addElement(((Content) i.next()).getValueObject());
+		}
 	}
 	
 	/**
 	 * 
 	 */
-	private final void initializeWithContentVO(final Collection elements) {
+	private final void initializeWithContentVO(final Collection elements) 
+	{
 		for(final Iterator i=elements.iterator(); i.hasNext(); )
+		{
 			addElement((ContentVO) i.next());
+		}
 	}
 	
 	/**
 	 * 
 	 */
-	private final void  initializeWithContentVersion(final Collection elements) {
+	private final void  initializeWithContentVersion(final Collection elements) 
+	{
 		for(final Iterator i=elements.iterator(); i.hasNext(); )
+		{
 			addElement(((ContentVersion) i.next()).getValueObject());
+		}
 	}
 	
 	/**
 	 * 
 	 */
-	private final void  initializeWithContentVersionVO(final Collection elements) {
+	private final void  initializeWithContentVersionVO(final Collection elements) 
+	{
 		for(final Iterator i=elements.iterator(); i.hasNext(); )
+		{
 			addElement((ContentVersionVO) i.next());
+		}
 	}
 	
 	/**
 	 * 
 	 */
-	public void addContentProperty(final String name, final boolean ascending) {
-		for(final Iterator i=structs.iterator(); i.hasNext(); ) 
-			((SortStruct) i.next()).addContentProperty(name, ascending);
-	}
-	
-	/**
-	 * 
-	 */
-	public void addContentVersionProperty(final String name, final boolean ascending) {
+	public void addContentProperty(final String name, final boolean ascending) 
+	{
 		for(final Iterator i=structs.iterator(); i.hasNext(); )
-			((SortStruct) i.next()).addContentVersionProperty(name, ascending);
+		{
+			((SortStruct) i.next()).addContentProperty(name, ascending);
+		}
 	}
 	
 	/**
 	 * 
 	 */
-	public void addContentVersionAttribute(final String name, final boolean ascending) {
+	public void addContentVersionProperty(final String name, final boolean ascending) 
+	{
+		for(final Iterator i=structs.iterator(); i.hasNext(); )
+		{
+			((SortStruct) i.next()).addContentVersionProperty(name, ascending);
+		}
+	}
+	
+	/**
+	 * 
+	 */
+	public void addContentVersionAttribute(final String name, final boolean ascending) 
+	{
 		addContentVersionAttribute(name, String.class, ascending);
 	}
 	
 	/**
 	 * 
 	 */
-	public void addContentVersionAttribute(final String name, final String className, final boolean ascending) {
-		try {
+	public void addContentVersionAttribute(final String name, final String className, final boolean ascending) 
+	{
+		try 
+		{
 			addContentVersionAttribute(name, Class.forName(className), ascending);
-		} catch(ClassNotFoundException e) {
+		} 
+		catch(ClassNotFoundException e) 
+		{
 			e.printStackTrace();
 			throw new IllegalArgumentException(e.getMessage());
 		}
@@ -316,32 +410,39 @@ public class ContentSort {
 	/**
 	 * 
 	 */
-	public void addContentVersionAttribute(final String name, final Class c, final boolean ascending) {
+	public void addContentVersionAttribute(final String name, final Class c, final boolean ascending) 
+	{
 		for(final Iterator i=structs.iterator(); i.hasNext(); )
+		{
 			((SortStruct) i.next()).addContentVersionAttribute(name, c, ascending);
+		}
 	}
 
 	/**
 	 * 
 	 */
-	private void addElement(final ContentVO contentVO) {
+	private void addElement(final ContentVO contentVO) 
+	{
 		structs.add(new SortStruct(controller, contentVO));
 	}
 	
 	/**
 	 * 
 	 */
-	private void addElement(final ContentVersionVO contentVersionVO) {
+	private void addElement(final ContentVersionVO contentVersionVO) 
+	{
 		structs.add(new SortStruct(controller, contentVersionVO));
 	}
 	
 	/**
 	 * 
 	 */
-	public List getContentResult() {
+	public List getContentResult() 
+	{
 		Collections.sort(structs);
 		final List result = new ArrayList();
-		for(final Iterator i=structs.iterator(); i.hasNext(); ) {
+		for(final Iterator i=structs.iterator(); i.hasNext(); ) 
+		{
 			final SortStruct struct = (SortStruct) i.next();
 			result.add(struct.getContentVO());
 		}
@@ -351,10 +452,12 @@ public class ContentSort {
 	/**
 	 * 
 	 */
-	public List getContentVersionResult() {
+	public List getContentVersionResult() 
+	{
 		Collections.sort(structs);
 		final List result = new ArrayList();
-		for(final Iterator i=structs.iterator(); i.hasNext(); ) {
+		for(final Iterator i=structs.iterator(); i.hasNext(); ) 
+		{
 			final SortStruct struct = (SortStruct) i.next();
 			result.add(struct.getContentVersionVO());
 		}
