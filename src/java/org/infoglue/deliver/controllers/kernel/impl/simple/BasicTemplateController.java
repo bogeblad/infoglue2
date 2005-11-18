ac03d19756e36f895f86517b72a6a65d9d10f084
mattias <mattias.bogeblad@knowit.se>
2005-11-18 08:15:11 +0000

    Fixed a escapeHTML method

--- src/java/org/infoglue/deliver/controllers/kernel/impl/simple/BasicTemplateController.java
+++ src/java/org/infoglue/deliver/controllers/kernel/impl/simple/BasicTemplateController.java
@@ -1301,6 +1301,17 @@ public class BasicTemplateController implements TemplateController
 		return attributeValue;
 	}
 
+
+	/**
+	 * This method deliveres a String with the content-attribute asked for in the language asked for.
+	 * As the siteNode can have multiple bindings as well as a content as a parameter this
+	 * parameter requires a bindingName which refers to the AvailableServiceBinding.name-attribute. 
+	 */
+	 
+	public String getContentAttributeValue(Integer contentId, String attributeName, boolean escapeHTML) 
+	{
+	    return getContentAttributeValue(contentId, this.getLanguageId(), attributeName, escapeHTML);
+	}
 	
 	/**
 	 * This method deliveres a String with the content-attribute asked for in the language asked for.
