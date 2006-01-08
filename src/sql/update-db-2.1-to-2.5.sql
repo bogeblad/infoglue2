d0080d55d4227994189a38738c0b8d0173f8796e
mattias <mattias.bogeblad@knowit.se>
2006-01-08 20:51:41 +0000

    Added scripts for upgrade to 2.5

---
+++ src/sql/update-db-2.1-to-2.5.sql
@@ -0,0 +1,34 @@
+-- ===============================================================================
+--
+-- Part of the InfoGlue Content Management Platform (www.infoglue.org)
+--
+-- ===============================================================================
+--
+--  Copyright (C)
+--
+-- This program is free software; you can redistribute it and/or modify it under
+-- the terms of the GNU General Public License version 2, as published by the
+-- Free Software Foundation. See the file LICENSE.html for more information.
+--
+-- This program is distributed in the hope that it will be useful, but WITHOUT
+-- ANY WARRANTY, including the implied warranty of MERCHANTABILITY or FITNESS
+-- FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License along with
+-- this program; if not, write to the Free Software Foundation, Inc. / 59 Temple
+-- Place, Suite 330 / Boston, MA 02111-1307 / USA.
+--
+-- ===============================================================================
+--
+-- $Id: update-db-2.1-to-2.5.sql,v 1.14 2005/06/29 08:52:46 mattias Exp $
+--
+-- This script contains the database updates required to go from 2.1 to 2.5.
+
+DROP TABLE IF EXISTS cmAccessRightUser;
+
+CREATE TABLE cmAccessRightUser (
+  accessRightUserId int(11) NOT NULL auto_increment,
+  accessRightId int(11) NOT NULL default '0',
+  userName varchar(150) NOT NULL default '',
+  PRIMARY KEY  (accessRightUserId)
+) TYPE=InnoDB;
