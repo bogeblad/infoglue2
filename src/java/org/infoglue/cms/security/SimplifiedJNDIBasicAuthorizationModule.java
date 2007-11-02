00d69ab605f3cc3baa7bf8011504ca442be276e1
mattias <mattias.bogeblad@knowit.se>
2007-11-02 09:41:33 +0000

    - Added feature in SimplifiedJNDIBasicAuthorizationModule so a users extra attributes can be read into the principal objects meta map. All attributes read are stored there.

--- src/java/org/infoglue/cms/security/SimplifiedJNDIBasicAuthorizationModule.java
+++ src/java/org/infoglue/cms/security/SimplifiedJNDIBasicAuthorizationModule.java
@@ -268,6 +268,7 @@ public class SimplifiedJNDIBasicAuthorizationModule implements AuthorizationModu
 				List groups = getGroups(userName, ctx);
 				
 				infogluePrincipal = new InfoGluePrincipal(userName, (String)userAttributes.get("displayName"), (String)userAttributes.get("firstName"), (String)userAttributes.get("lastName"), (String)userAttributes.get("mail"), roles, groups, isAdministrator, this);
+				infogluePrincipal.getMetaInformation().putAll(userAttributes);
 				
 			    if(infogluePrincipal != null)
 			    	CacheController.cacheObjectInAdvancedCache("JNDIAuthorizationCache", key, infogluePrincipal, null, false);
