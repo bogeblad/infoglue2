e4ca2717361efad21ffd06aa0468d0866e5588bf
mattias <mattias.bogeblad@knowit.se>
2008-06-13 14:41:20 +0000

    Removed autogrow plugin

--- src/webapp/cms/contenttool/WYSIWYGConfig.js
+++ src/webapp/cms/contenttool/WYSIWYGConfig.js
@@ -49,9 +49,9 @@ FCKConfig.PreloadImages = [ FCKConfig.SkinPath + 'images/toolbar.start.gif', FCK
 
 FCKConfig.PluginsPath = FCKConfig.BasePath + 'plugins/' ;
 
-// FCKConfig.Plugins.Add( 'autogrow' ) ;
+//FCKConfig.Plugins.Add( 'autogrow' ) ;
 // FCKConfig.Plugins.Add( 'dragresizetable' );
-FCKConfig.AutoGrowMax = 400 ;
+//FCKConfig.AutoGrowMax = 2000 ;
 
 // FCKConfig.ProtectedSource.Add( /<%[\s\S]*?%>/g ) ;	// ASP style server side code <%...%>
 // FCKConfig.ProtectedSource.Add( /<\?[\s\S]*?\?>/g ) ;	// PHP style server side code
