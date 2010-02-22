addcb50d03c08baf37513e7d03fc1c0878b31778
mattias <mattias.bogeblad@knowit.se>
2010-02-22 15:02:05 +0000

    Improved monitor mails to include accurate memory figures when the thread started (not when finished)

--- src/java/org/infoglue/deliver/util/ThreadMonitor.java
+++ src/java/org/infoglue/deliver/util/ThreadMonitor.java
@@ -24,11 +24,14 @@
 package org.infoglue.deliver.util;
 
 import java.io.*;
+import java.lang.Thread.State;
 import java.lang.management.ManagementFactory;
 import java.lang.management.ThreadInfo;
 import java.lang.management.ThreadMXBean;
 import java.net.InetAddress;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -44,22 +47,33 @@ import org.infoglue.cms.util.mail.MailServiceFactory;
 
 public class ThreadMonitor implements Runnable
 {
-
     private final static Logger logger = Logger.getLogger(ThreadMonitor.class.getName());
 
+    private static long lastThreadDump = -1;
+    
 	private Thread targetThread;
 	private long millis;
 	private long started;
 	private Thread watcherThread;
-	private boolean loop;
-	private boolean enabled;
+	
+	private final AtomicBoolean isDoneRunning;
+	private final AtomicBoolean loop;
+	private final AtomicBoolean enabled;
 	private HttpServletRequest request;
 	private String message;
 	private boolean kill = false;
 	private long threadId;
 	private static long lastSentTimer = System.currentTimeMillis();
 	private long startTimeInNs;
+
+	private long totalMemory;
+	private long freeMemory;
+	private long maxMemory;
 		
+	private int numberOfCurrentRequests = 0;
+	private int numberOfActiveRequests = 0;
+	private int longThreadMonitorsSize = 0;
+	
 	/// Constructor.  Give it a thread to watch, and a timeout in milliseconds.
 	// After the timeout has elapsed, the thread gets killed.  If you want
 	// to cancel the kill, just call done().
@@ -69,12 +83,22 @@ public class ThreadMonitor implements Runnable
 		this.millis = millis;
 		this.started = System.currentTimeMillis();
 		watcherThread = new Thread(this);
-		enabled = true;
+		isDoneRunning = new AtomicBoolean(false);
+		loop = new AtomicBoolean(true);
+		enabled = new AtomicBoolean(true);
 		this.request = request;
 		this.message = message;
 		this.kill = kill;
 		this.threadId = Thread.currentThread().getId();
 		
+		this.freeMemory = Runtime.getRuntime().freeMemory();
+		this.totalMemory = Runtime.getRuntime().totalMemory();
+		this.maxMemory = Runtime.getRuntime().maxMemory();
+		
+		this.numberOfCurrentRequests = RequestAnalyser.getRequestAnalyser().getNumberOfCurrentRequests();
+		this.numberOfActiveRequests	 = RequestAnalyser.getRequestAnalyser().getNumberOfActiveRequests();
+		this.longThreadMonitorsSize  = RequestAnalyser.getLongThreadMonitors().size();
+
 		if(millis > 0)
 			watcherThread.start();
 
@@ -99,20 +123,20 @@ public class ThreadMonitor implements Runnable
 	/// Call this when the target thread has finished.
 	public synchronized void done()
 	{
-		loop = false;
-		enabled = false;
+		loop.set(false);
+		enabled.set(false);
 		notify();
 	}
 
 	/// Call this to restart the wait from zero.
-	public synchronized void reset()
+	public void reset()
 	{
-		loop = true;
+		loop.set(true);
 		notify();
 	}
 
 	/// Call this to restart the wait from zero with a different timeout value.
-	public synchronized void reset(long millis)
+	public void reset(long millis)
 	{
 		this.millis = millis;
 		reset();
@@ -129,11 +153,11 @@ public class ThreadMonitor implements Runnable
 		ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
     	startTimeInNs = threadMXBean.getCurrentThreadCpuTime();
 
-		if (enabled)
+		if (enabled.get())
 		{
 			do
 			{
-				loop = false;
+				loop.set(false);
 				try
 				{
 					wait(millis);
@@ -142,18 +166,22 @@ public class ThreadMonitor implements Runnable
 				{
 				}
 			} 
-			while (enabled && loop);
+			while (enabled.get() && loop.get());
 		}
 		
-		if (enabled && targetThread.isAlive())
+		if (enabled.get() && targetThread.isAlive())
 		{
+			isDoneRunning.set(true);
+
 			printThread();
 			if(kill)
 			{
-				logger.error("Trying to kill thread with id:" + threadId);
+				logger.warn("Trying to kill thread with id:" + targetThread.getId());
 				//targetThread.interrupt();
-				//targetThread.stop();
+				targetThread.stop();
 			}
+			done();
+			isDoneRunning.set(false);
 		}
 	}
 
@@ -162,32 +190,140 @@ public class ThreadMonitor implements Runnable
 	{
 		long now = System.currentTimeMillis();
 		
+		long diffLastThreadDump = now - lastThreadDump;
+		logger.info("diffLastThreadDump:" + diffLastThreadDump);
+		if(diffLastThreadDump > 600000)
+		{
+			logger.info("had not sent all threads for a while.. will do so now");
+			lastThreadDump = now;
+			
+			try
+			{
+				ThreadMXBean t = ManagementFactory.getThreadMXBean();
+		        long threads[] = t.getAllThreadIds();
+		        ThreadInfo[] tinfo = t.getThreadInfo(threads, 40);
+		        
+		        StringBuilder sb = new StringBuilder("All Threads");
+				
+		        for (int i=0; i<tinfo.length; i++)
+		        {
+	                ThreadInfo e = tinfo[i];
+	                
+	                try
+	                {
+		                StackTraceElement[] el = e.getStackTrace();
+		                sb.append("\n\n" + e.getThreadName() + "\n" + " " + " Thread id = " + e.getThreadId() + " " + e.getThreadState());
+		                if(e.getThreadState().equals(State.BLOCKED))
+		                {
+		                	sb.append("\n\nBlocked info: " + e.getBlockedCount() + ":" + e.getBlockedTime() + ":" + e.getLockName() + ":" + e.getLockOwnerId() + ":" + e.getLockOwnerName() + "\n" + " " + " Thread id = " + e.getThreadId() + " " + e.getThreadState());
+		    		        
+		                	ThreadInfo eBlockedThread = t.getThreadInfo(e.getLockOwnerId(), 40);
+	    	                StackTraceElement[] elBlockedThread = eBlockedThread.getStackTrace();
+	    	                sb.append("\n\n    " + e.getThreadName() + "\n" + " " + " Thread id = " + eBlockedThread.getThreadId() + " " + eBlockedThread.getThreadState());
+	    	                
+	    	                if (elBlockedThread == null || elBlockedThread.length == 0)
+	    	                {
+	                    		sb.append("        no stack trace available");
+	                    	}
+	    	                else
+	    	                {
+		    	                for (int n = 0; n < elBlockedThread.length; n++)
+		    	                {
+		                            if (n != 0)
+		                            	sb.append("\n");
+		
+		                            StackTraceElement frame = elBlockedThread[n];
+		
+		                            if (frame == null) {
+		                            	sb.append("        null stack frame");
+		                                continue;
+		                            }
+		
+		                            sb.append("        ");
+		                            sb.append(frame.toString());
+		    	                }
+	    	                }
+	    	            }
+		
+		                if (el == null || el.length == 0)
+		                {
+	                		sb.append("    no stack trace available");
+	                		continue;
+		               	}
+		
+		                for (int n = 0; n < el.length; n++)
+		                {
+	                        if (n != 0)
+	                        	sb.append("\n");
+	
+	                        StackTraceElement frame = el[n];
+	
+	                        if (frame == null) {
+	                        	sb.append("    null stack frame");
+	                            continue;
+	                        }
+	
+	                        sb.append("    ");
+	                        sb.append(frame.toString());
+		                }
+	                }
+	                catch(Exception e2)
+	                {
+	                }
+		        }
+		        //System.out.println("Stack:\n" + sb.toString());
+		        //logger.error("Stack:" + sb.toString());
+
+		        String warningEmailReceiver = CmsPropertyHandler.getWarningEmailReceiver();
+		        if(warningEmailReceiver != null && !warningEmailReceiver.equals("") && warningEmailReceiver.indexOf("@warningEmailReceiver@") == -1)
+		        {
+					try
+					{
+				        logger.info("Mailing..");
+						MailServiceFactory.getService().sendEmail(CmsPropertyHandler.getMailContentType(), warningEmailReceiver, warningEmailReceiver, null, null, null, null, message, sb.toString().replaceAll("\n", "<br/>"), "utf-8");
+					} 
+					catch (Exception e)
+					{
+						logger.error("Could not send mail:" + e.getMessage(), e);
+					}
+		        }
+			}
+			catch (Throwable e) 
+			{
+				logger.error("Error generating message:" + e.getMessage(), e);
+			}
+				        
+		}
+
 		//Only sends if the last stack was sent more than 3 seconds ago.
-		if((now - lastSentTimer) > 3000)
+		if((now - lastSentTimer) > 10000)
 		{			
+	        lastSentTimer = System.currentTimeMillis();
+
 	    	StackTraceElement[] el = targetThread.getStackTrace();
 
 	        ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
-        	long stopTimeInNs = threadMXBean.getCurrentThreadCpuTime();
+        	long stopTimeInNs = threadMXBean.getThreadUserTime(targetThread.getId());
 	        long diff = (stopTimeInNs - startTimeInNs) / 1000000000;
         	//System.out.println("diff:" + diff);
         	
 	        StringBuffer stackString = new StringBuffer("\n\n" + message + "\n\n");
 	        stackString.append("ServerName: " + getServerName() + "\n");
-	        stackString.append("Maximum memory (MB): " + (Runtime.getRuntime().maxMemory() / 1024 / 1024) + "\n");
-	        stackString.append("Used memory (MB): " + ((Runtime.getRuntime().totalMemory()- Runtime.getRuntime().freeMemory()) / 1024 / 1024) + "\n");
-	        stackString.append("Free memory (MB): " + (Runtime.getRuntime().freeMemory() / 1024 / 1024) + "\n");
-	        stackString.append("Total memory (MB): " + (Runtime.getRuntime().totalMemory() / 1024 / 1024) + "\n");
-	        stackString.append("Number of current requests: " + RequestAnalyser.getRequestAnalyser().getNumberOfCurrentRequests() + "\n");
-	        stackString.append("Number of active requests: " + RequestAnalyser.getRequestAnalyser().getNumberOfActiveRequests() + "\n");
-	        stackString.append("Number of long requests: " + RequestAnalyser.getLongThreadMonitors().size() + "\n");
+	        stackString.append("Maximum memory (MB): " + (maxMemory / 1024 / 1024) + "\n");
+	        stackString.append("Used memory (MB): " + ((totalMemory - freeMemory) / 1024 / 1024) + "\n");
+	        stackString.append("Free memory (MB): " + (freeMemory / 1024 / 1024) + "\n");
+	        stackString.append("Total memory (MB): " + (totalMemory / 1024 / 1024) + "\n");
+	        stackString.append("Number of current requests: " + numberOfCurrentRequests + "\n");
+	        stackString.append("Number of active requests: " + numberOfActiveRequests + "\n");
+	        stackString.append("Number of long requests: " + longThreadMonitorsSize + "\n");
 	        stackString.append("Current thread time: " + diff + " seconds\n");
 	        stackString.append("Average time: " + RequestAnalyser.getRequestAnalyser().getAverageElapsedTime() + "\n");
 	        stackString.append("Longest time: " + RequestAnalyser.getRequestAnalyser().getMaxElapsedTime() + "\n");
 	        stackString.append("Original url: " + getOriginalFullURL() + "\n");
 	        stackString.append("UserInfo: " + getUserInfo() + "\n");
 	        stackString.append("--------------------------------------------\n\n");
-	        stackString.append("Thread with id [" + threadId + "] at report time:\n");
+	        stackString.append("Thread with id [" + targetThread.getId() + "] at report time:\n");
+
 	        if (el != null && el.length != 0)
 	        {
 	            for (int j = 0; j < el.length; j++)
@@ -203,6 +339,41 @@ public class ThreadMonitor implements Runnable
 				}                    
 	       	}
 	
+	        if(targetThread.getState().equals(State.BLOCKED))
+            {
+				ThreadMXBean t = ManagementFactory.getThreadMXBean();
+				
+				ThreadInfo e = t.getThreadInfo(targetThread.getId(), 40);
+	        	stackString.append("\n\nBlocked info: " + e.getBlockedCount() + ":" + e.getBlockedTime() + ":" + e.getLockName() + ":" + e.getLockOwnerId() + ":" + e.getLockOwnerName() + "\n" + " " + " Thread id = " + e.getThreadId() + " " + e.getThreadState());
+		        
+            	ThreadInfo eBlockedThread = t.getThreadInfo(e.getLockOwnerId(), 40);
+                StackTraceElement[] elBlockedThread = eBlockedThread.getStackTrace();
+                stackString.append("\n\nBlocked thread: " + e.getThreadName() + "\n" + " " + " Thread id = " + eBlockedThread.getThreadId() + " " + eBlockedThread.getThreadState());
+                
+                if (elBlockedThread == null || elBlockedThread.length == 0)
+                {
+                	stackString.append("        no stack trace available");
+            	}
+                else
+                {
+	                for (int n = 0; n < elBlockedThread.length; n++)
+	                {
+                        if (n != 0)
+                        	stackString.append("\n");
+
+                        StackTraceElement frame = elBlockedThread[n];
+
+                        if (frame == null) {
+                        	stackString.append("        null stack frame");
+                            continue;
+                        }
+
+                        stackString.append("        ");
+                        stackString.append(frame.toString());
+	                }
+                }
+            }
+	        
 			stackString.append("\n\n**********************************\nConcurrent long threads (Only an excerpt of all)\n**********************************");
 	
 		    ThreadMXBean t = ManagementFactory.getThreadMXBean();
@@ -214,11 +385,11 @@ public class ThreadMonitor implements Runnable
 		    {
 				ThreadMonitor tm = (ThreadMonitor)threadMonitorsIterator.next();
 				
-				if(threadId == tm.getThreadId())
+				if(targetThread.getId() == tm.getThreadId())
 					continue;
 				
 				long threads[] = {tm.getThreadId()};
-			    ThreadInfo[] tinfo = t.getThreadInfo(threads, 20);
+			    ThreadInfo[] tinfo = t.getThreadInfo(threads, 40);
 	
 				stackString.append("\n\n---------------------------------\nConcurrent long thread [").append(tm.getThreadId()).append("]:\n");
 				stackString.append("Elapsed time:").append(tm.getElapsedTime()).append("\n Thread id: ").append(tm.getThreadId()).append("\n Original url: ").append(tm.getOriginalFullURL()).append(")");
@@ -260,7 +431,6 @@ public class ThreadMonitor implements Runnable
 				}
 	        }
 
-	        lastSentTimer = System.currentTimeMillis();
 		}
 		else
 		{
@@ -335,10 +505,14 @@ public class ThreadMonitor implements Runnable
 	{
 		return System.currentTimeMillis() - this.started;
 	}
-
+	
 	public long getThreadId() 
 	{
 		return threadId;
 	}
-
+	
+	public boolean getIsDoneRunning()
+	{
+		return this.isDoneRunning.get();
+	}
 }
