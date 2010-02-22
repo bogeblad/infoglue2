4a58863149579745c162d1d53d9adaea5e570136
mattias <mattias.bogeblad@knowit.se>
2010-02-22 08:19:32 +0000

    New column

--- src/sql/update-db-2.9-to-3.0.sql
+++ src/sql/update-db-2.9-to-3.0.sql
@@ -4,17 +4,31 @@ ALTER TABLE cmSubscription change entityId entityName varchar(200) DEFAULT NULL;
 
 ALTER TABLE cmDigAsset MODIFY ( assetContentType VARCHAR2(255) );
 
-alter table cmDigAsset modify assetContentType 
-
-
 --ALTER TABLE cmSiteNode ADD COLUMN sortOrder INTEGER NOT NULL DEFAULT '-1';
 --ALTER TABLE cmSiteNode ADD COLUMN isHidden TINYINT NOT NULL DEFAULT 0;
 
 ALTER TABLE cmSiteNodeVersion ADD COLUMN sortOrder INT(10) NOT NULL DEFAULT -1;
 ALTER TABLE cmSiteNodeVersion ADD COLUMN isHidden TINYINT UNSIGNED NOT NULL DEFAULT 0;
+ALTER TABLE cmSiteNodeVersion ADD COLUMN forceProtocolChange TINYINT(4) UNSIGNED NOT NULL DEFAULT 2;
 
 ALTER TABLE cmSiteNode ADD COLUMN isDeleted TINYINT NOT NULL DEFAULT 0;
 ALTER TABLE cmContent ADD COLUMN isDeleted TINYINT NOT NULL DEFAULT 0;
 ALTER TABLE cmRepository ADD COLUMN isDeleted TINYINT NOT NULL DEFAULT 0;
 
 OBS::::::: add index på alla isDeleted, sortOrder och isHidden
+
+drop index propCategoryAttrNameIndex;
+drop index propCategoryEntityNameIndex;
+drop index propCategoryEntityNameIndex;
+drop index propCategoryCategoryIdIndex;
+drop index categoryParentIdIndex;
+drop index categoryNameIndex;
+
+create index propCategoryAttrNameIndex on cmPropertiesCategory(attributeName(100));
+create index propCategoryEntityNameIndex on cmPropertiesCategory(entityName(100));
+create index propCategoryEntityIdIndex on cmPropertiesCategory(entityId(255));
+create index propCategoryCategoryIdIndex on cmPropertiesCategory(categoryId);
+create index categoryParentIdIndex on cmCategory(parentId);
+create index categoryNameIndex on cmCategory(name(100));
+
+
