fb6e34f68d4d8cf24dd385df582adaf68c3c8546
mattias <mattias.bogeblad@knowit.se>
2012-08-21 22:09:20 +0000

    Backed back to 2.10.0.3 but with most 2.10.10.0 additions.

---
+++ src/java/org/infoglue/cms/security/BasicAuthorizationModule.java
@@ -0,0 +1,16 @@
+package org.infoglue.cms.security;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Mattias Bogeblad
+ *
+ * This authorization module can be used by all Authorization modules that cannot provide advanced paging/search directly from the source.
+ */
+
+public abstract class BasicAuthorizationModule implements AuthorizationModule
+{
+	
+
+}
---
+++ src/java/org/infoglue/cms/security/BasicIndexedAuthorizationModule.java
@@ -0,0 +1,17 @@
+package org.infoglue.cms.security;
+
+
+/**
+ * @author Mattias Bogeblad
+ *
+ * This authorization module can be used by all Authorization modules that cannot provide advanced paging/search directly from the source but
+ * wants a search enabled / paged experience anyway. Works on cached data and the update interval can be decided by using the normal extra security settings (parameter name 'indexTimeout').
+ */
+
+public abstract class BasicIndexedAuthorizationModule
+{
+    
+
+}
+
+
