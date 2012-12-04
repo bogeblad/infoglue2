a45aba4098cea8641574109eb3ffde96bad87375
mattias <mattias.bogeblad@knowit.se>
2012-12-04 14:16:01 +0000

    Removed deebug

--- src/java/org/infoglue/cms/controllers/kernel/impl/simple/ExtendedSearchController.java
+++ src/java/org/infoglue/cms/controllers/kernel/impl/simple/ExtendedSearchController.java
@@ -195,7 +195,7 @@ public class ExtendedSearchController extends BaseController
 		    if(logger.isDebugEnabled())
 		    	logger.debug("sql:" + sqlBuilder.getSQL());
 		    
-		    System.out.println("SQL:" + sqlBuilder.getSQL());
+		    //System.out.println("SQL:" + sqlBuilder.getSQL());
 		    
 			final OQLQuery oql = db.getOQLQuery(sqlBuilder.getSQL());
 			for(Iterator i=sqlBuilder.getBindings().iterator(); i.hasNext(); )
@@ -203,7 +203,7 @@ public class ExtendedSearchController extends BaseController
 			    Object o = i.next();
 			    if(logger.isDebugEnabled())
 			    	logger.debug("o:" + o.toString());
-			    System.out.println("o:" + o.toString());
+			    //System.out.println("o:" + o.toString());
 			    oql.bind(o);
 			}
 			
