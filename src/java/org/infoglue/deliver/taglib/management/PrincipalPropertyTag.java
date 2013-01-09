72232d93884225a9e0cc8fb9c0218a3b48cb41d5
mattias <mattias.bogeblad@knowit.se>
2013-01-09 00:33:34 +0000

    Fixed check so anonymous is not allowed to have properties by default

--- src/java/org/infoglue/deliver/taglib/management/PrincipalPropertyTag.java
+++ src/java/org/infoglue/deliver/taglib/management/PrincipalPropertyTag.java
@@ -30,21 +30,28 @@ import java.util.Map;
 
 import javax.servlet.jsp.JspException;
 
+import org.apache.log4j.Logger;
 import org.infoglue.cms.entities.management.impl.simple.GroupPropertiesImpl;
 import org.infoglue.cms.entities.management.impl.simple.RolePropertiesImpl;
 import org.infoglue.cms.entities.management.impl.simple.UserPropertiesImpl;
 import org.infoglue.cms.security.InfoGluePrincipal;
+import org.infoglue.cms.util.CmsPropertyHandler;
 import org.infoglue.deliver.taglib.TemplateControllerTag;
 
 public class PrincipalPropertyTag extends TemplateControllerTag 
 {
+    private final static Logger logger = Logger.getLogger(PrincipalPropertyTag.class.getName());
+
 	private static final long serialVersionUID = 4050206323348354355L;
 
 	private String userName;
 	private InfoGluePrincipal principal;
 	private String attributeName;
-	private Integer languageId 		= null;
-    private boolean defeatCaches	= false;
+	private Integer languageId 				= null;
+    private boolean defeatCaches			= false;
+    private boolean allowAnonymousProperty 	= false;
+    private boolean includeRoles 			= true;
+    private boolean includeGroups 			= true;
 
     public PrincipalPropertyTag()
     {
@@ -65,15 +72,27 @@ public class PrincipalPropertyTag extends TemplateControllerTag
 	        entities.put(GroupPropertiesImpl.class, Collections.EMPTY_LIST);
 	        entities.put(RolePropertiesImpl.class, Collections.EMPTY_LIST);
 	        getController().getDeliveryContext().setDefeatCaches(defeatCaches, entities);
-	
+
 		    if(userName != null && !userName.equals(""))
 		    {
-		        setResultAttribute(this.getController().getPrincipalPropertyValue(getController().getPrincipal(userName), attributeName, languageId));
+				if(!allowAnonymousProperty && userName.equalsIgnoreCase(CmsPropertyHandler.getAnonymousUser()))
+				{
+					setResultAttribute("Anonymous not allowed to have properties");
+					logger.warn("Anonymous not allowed to have properties unless stated. URL:" + this.getController().getOriginalFullURL() + "\nComponentName:" + this.getController().getComponentLogic().getInfoGlueComponent().getName());
+				}
+				else
+					setResultAttribute(this.getController().getPrincipalPropertyValue(getController().getPrincipal(userName), attributeName, languageId));
 		    }
 		    else if(principal != null)
 		    {
-	            setResultAttribute(getController().getPrincipalPropertyValue(principal, attributeName, languageId));
-		    }
+				if(!allowAnonymousProperty && principal.getName().equalsIgnoreCase(CmsPropertyHandler.getAnonymousUser()))
+				{
+					setResultAttribute("");
+					logger.warn("Anonymous not allowed to have properties unless stated. URL:" + this.getController().getOriginalFullURL() + "\nComponentName:" + this.getController().getComponentLogic().getInfoGlueComponent().getName());
+				}
+				else
+					setResultAttribute(getController().getPrincipalPropertyValue(principal, attributeName, languageId));
+			}
 		    else
 		    {
 		    	setResultAttribute(getController().getPrincipalPropertyValue(attributeName, languageId));
@@ -87,6 +106,9 @@ public class PrincipalPropertyTag extends TemplateControllerTag
 	        userName 		= null;
 	        principal 		= null;
 		    defeatCaches 	= false;
+		    allowAnonymousProperty = false;
+		    includeRoles = false;
+		    includeGroups = false;
 		}
 		
         return EVAL_PAGE;
@@ -112,6 +134,21 @@ public class PrincipalPropertyTag extends TemplateControllerTag
  	   this.languageId = this.evaluateInteger("principal", "languageId", languageIdString);
     }
     
+    public void setIncludeRoles(final String includeRoles) throws JspException
+    {
+        this.includeRoles = (Boolean)evaluate("principal", "includeRoles", includeRoles, Boolean.class);
+    }
+
+    public void setIncludeGroups(final String includeGroups) throws JspException
+    {
+        this.includeGroups = (Boolean)evaluate("principal", "includeGroups", includeGroups, Boolean.class);
+    }
+
+    public void setAllowAnonymousProperty(final String allowAnonymousProperty) throws JspException
+    {
+        this.allowAnonymousProperty = (Boolean)evaluate("principal", "allowAnonymousProperty", allowAnonymousProperty, Boolean.class);
+    }
+
     public void setDefeatCaches(final String defeatCaches) throws JspException
     {
         this.defeatCaches = (Boolean)evaluate("principal", "defeatCaches", defeatCaches, Boolean.class);
